8d643e287a2e5c89dd5c2aa2727233e438a6d20a
aqib-pervaiz <butaqib123@gmail.com>
2024-07-30 01:40:09 +0500

    uploadToDrive

--- downloadImages.py
+++ downloadImages.py
@@ -2,19 +2,26 @@ import requests
 import json
 import os
 
-folder_path="All Images"
+from GoogleDriveHelper import GoogleDriverHelper
+
+helper = GoogleDriverHelper("Scag Images")
+folder_path = "All Images"
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
-
+list_error_images = []
 with open("images.json", "r") as all_images_data:
     all_data = json.load(all_images_data)
 
-for image in all_data[0:5]:
+for image in all_data[0:1]:
     try:
         response = requests.get(image["image_url"])
-        with open(f"All Images/{image["file_name"]}", "wb") as binaryimage:
-            binaryimage.write(response.content)
+        helper.upload_file_from_content(response.content, image["file_name"])
         print(response)
-        
+
     except Exception as e:
+        list_error_images.append(image)
+        print(e)
         print(f"Error downloading image {image}")
+
+with open("ErrorImages.josn", "w") as filename:
+    filename.write(json.dumps(list_error_images, indent=4))
