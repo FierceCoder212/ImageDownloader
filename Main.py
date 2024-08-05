0dccfcab4787dbc77eda636478e8174742f94b53
AbdullahFarooq123 <abdullahfarooq20172018@gmail.com>
2024-08-06 00:20:10 +0500

    Fixed drive loading issue

--- Main.py
+++ Main.py
@@ -52,26 +52,26 @@ def download_and_upload_image(image):
         os.remove(file_name)
 
 
-helper.save_prev_drive_files_on_folder()
+# helper.save_prev_drive_files_on_folder()
 
-# with open("images.json", "r") as all_images_data:
-#     all_data = json.load(all_images_data)
-# with open("Uploaded Files.json", "r") as all_uploaded_images:
-#     all_uploaded_data = json.load(all_uploaded_images)
-#
-# all_data = [data for data in all_data if data["file_name"] not in all_uploaded_data]
-# list_error_images = []
-#
-# # Create a ThreadPoolExecutor
-# with ThreadPoolExecutor(max_workers=10) as executor:
-#     # Submit tasks to the executor
-#     futures = [executor.submit(download_and_upload_image, image) for image in all_data]
-#
-#     # Process the results as they complete
-#     for future in as_completed(futures):
-#         if error_image := future.result():
-#             list_error_images.append(error_image)
-#
-# # Save the error images to a file
-# with open("ErrorImages.json", "w") as filename:
-#     filename.write(json.dumps(list_error_images, indent=4))
+with open("images.json", "r") as all_images_data:
+    all_data = json.load(all_images_data)
+with open("Uploaded Files.json", "r") as all_uploaded_images:
+    all_uploaded_data = json.load(all_uploaded_images)
+
+all_data = [data for data in all_data if data["file_name"] not in all_uploaded_data]
+list_error_images = []
+
+# Create a ThreadPoolExecutor
+with ThreadPoolExecutor(max_workers=10) as executor:
+    # Submit tasks to the executor
+    futures = [executor.submit(download_and_upload_image, image) for image in all_data]
+
+    # Process the results as they complete
+    for future in as_completed(futures):
+        if error_image := future.result():
+            list_error_images.append(error_image)
+
+# Save the error images to a file
+with open("ErrorImages.json", "w") as filename:
+    filename.write(json.dumps(list_error_images, indent=4))
