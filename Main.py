4afe731ccc6b6d535323f3bf91a0a02d6e65e01d
AbdullahFarooq123 <abdullahfarooq20172018@gmail.com>
2024-10-30 01:02:53 +0500

    Init

--- Main.py
+++ Main.py
@@ -17,7 +17,7 @@ sql_lite_helper = SQLiteHelper('Images.db')
 
 # helper.save_prev_drive_files_on_folder()
 for section_diagram, base_64_img in sql_lite_helper.yield_fetch_all_records():
+    base_64_img = base_64_img.replace('data:image/PNG;base64,', '')
     image_data = base64.b64decode(base_64_img)
-    file_name = f'{section_diagram}.png'
-    google_drive_helper.upload_file_from_content(file_bytes=image_data, file_name=file_name)
-    print(f'Uploaded {file_name} to Google Drive.')
+    google_drive_helper.upload_file_from_content(file_bytes=image_data, file_name=section_diagram)
+    print(f'Uploaded {section_diagram} to Google Drive.')
